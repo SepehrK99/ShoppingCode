21419bfcd5a8b81ab797d1221ec302ea8a6b29d4
Sepehr Kaboodband <sepehr.k@outlook.de>
2022-09-27 18:02:32 +0200

    login

--- ShoppingCode/src/app/app-routing.module.ts
+++ ShoppingCode/src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {SocialLoginModule, SocialAuthServiceConfig,} from 'angularx-social-login';
+
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, SocialLoginModule],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('Google-Client-ID-Goes-Here'),
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('clientId')
+          }
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
 
 const routes: Routes = [];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- ShoppingCode/src/app/google/google.component.ts
+++ ShoppingCode/src/app/google/google.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {SocialAuthService, GoogleLoginProvider, SocialUser} from 'angularx-social-login';
 @Component({
-  selector: 'app-google',
-  templateUrl: './google.component.html',
-  styleUrls: ['./google.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class GoogleComponent implements OnInit {
-
-  constructor() {
-   }
-
-  ngOnInit(): void {
+export class AppComponent implements OnInit {
+  loginForm!: FormGroup;
+  socialUser!: SocialUser;
+  isLoggedin?: boolean;
+  constructor(
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService
+  ) {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.socialAuthService.authState.subscribe((user: null) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+      console.log(this.socialUser);
+    });
+  }
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  logOut(): void {
+    this.socialAuthService.signOut();
   }
 
+  // 767282381505-v7lvifavs4eo0t456b15qa4rlg9e0i54.apps.googleusercontent.com
+
+  // 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
 }
